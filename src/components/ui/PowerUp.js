6c701bfde82dd67d875fcd44f012bd8d3a8010ed
郑子华 <zhengzihua@100tal.com>
2018-07-16 13:44:02 +0800

    create powerup.js

--- src/components/ui/PowerUp.js
+++ src/components/ui/PowerUp.js
@@ -1,11 +1,39 @@
 import {Container} from 'pixi.js'
 import {getAnimation} from "../resource";
-class PowerUp extends Container{
-    constructor() {
-      super();
-      this.powerup = getAnimation('animation_train_json');
-      this.powerup.state.setAnimation(0, 'train_top').loop = true;
-    }
+
+class PowerUp extends Container {
+  animate;
+  slot;
+
+  constructor() {
+    super();
+    this.animate = getAnimation('animation_powerup_json');
+    this.slot = this.animate.skeleton.findSlot('text');
+    this.addChild(this.animate);
+    this.animate.state.setAnimation(0, 'bounce').loop = true;
+    this.animate.x = 1920 >> 1;
+    this.animate.y = 1080 >> 1;
+
+
+  }
+
+  _performAnimate(str) {
+    let txtAttach = this.animate.skeleton.getAttachment(this.slot.data.index, str);
+    this.slot.setAttachment(txtAttach);
+  }
+
+  timeOver() {
+    this._performAnimate('')
+  }
+
+  success() {
+
+  }
+
+  falit() {
+
+  }
+
 }
 
 export {PowerUp}
